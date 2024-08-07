bc39102d47a7d439299fb72db1543f96e02c3ff2
xcclyc <yiai_mincookook@qq.com>
2024-08-07 13:52:16 +0800

    Add files via upload

---
+++ 关闭极城.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	Sleep(1000);
+	system("TASKKILL /F /IM StudentMain.exe /T");
+	return 0;
+}
---
+++ 极域杀手.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+using namespace std;
+int main(){
+	Sleep(1500);cout << "欢迎使用极域杀手" << endl;
+	Sleep(1000);
+	Sleep(1000);
+	cout << "进入辅助网站，请等待";
+	for(int i=1;i<=6;i++){Sleep(300);cout<<".";}cout << endl;
+	system("pause");
+	system("ipconfig /all");
+	Sleep(200);system("arp /a");
+	Sleep(200);system("net start");Sleep(200);system("net user");
+	Sleep(200);system("net use");Sleep(200);system("net share");
+	Sleep(200);system("net file");Sleep(200);system("net config");
+	Sleep(1000);system("dir /s /r /d /1 /r /a /o /b");
+	Sleep(1000);system("tasklist");
+	Sleep(1000);system("TASKKILL /F /IM StudentMain.exe /T");
+	Sleep(1000);cout<<"已无极域威胁."<<endl;Sleep(1000);
+	Sleep(1008);
+	Sleep(2000);Sleep(2000);
+	Sleep(2000);MessageBox(NULL,"你可以安心做自己的事情了^_^","极域杀手",MB_OK);
+	return 0;
+}
